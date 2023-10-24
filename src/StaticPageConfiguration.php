b389b658f727c2b28b60b2676d3f7a964ef01bc5
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2023-10-24 20:14:02 +0200

    chore: remove unused settings from the static pages config

--- src/StaticPageConfiguration.php
+++ src/StaticPageConfiguration.php
@@ -8,11 +8,7 @@ use Ds\Map;
 
 readonly class StaticPageConfiguration
 {
-    /**
-     * @param Map<string,string> $globals
-     */
     public function __construct(
-        public Map $globals,
         public string $baseUrl,
         public string $esbuildMetafile,
         public string $inputDirectory,
