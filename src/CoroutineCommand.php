2aabef7958364be053731d50d293a3df0cea8043
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2023-12-11 10:06:30 +0100

    chore: add coroutine command

---
+++ src/CoroutineCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Distantmagic\Resonance;
+
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use function Swoole\Coroutine\run;
+
+abstract class CoroutineCommand extends SymfonyCommand
+{
+    abstract protected function executeInCoroutine(InputInterface $input, OutputInterface $output): int;
+
+    /**
+     * Do not use Symfony's command constructor arguments to make it easier on
+     * the DI.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $result = 0;
+
+        /**
+         * @var bool
+         */
+        $coroutineResult = run(function () use ($input, $output, &$result) {
+            $result = $this->executeInCoroutine($input, $output);
+        });
+
+        if (!$coroutineResult) {
+            return Command::FAILURE;
+        }
+
+        if (is_int($result)) {
+            return $result;
+        }
+
+        return Command::SUCCESS;
+    }
+}
