a95ae793787ec282cc5b3e1e5d90410cddff36d6
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2023-12-14 08:26:16 +0100

    chore: remove coroutine call from static page processor

--- src/StaticPageProcessor.php
+++ src/StaticPageProcessor.php
@@ -5,12 +5,8 @@ declare(strict_types=1);
 namespace Distantmagic\Resonance;
 
 use Distantmagic\Resonance\Attribute\Singleton;
-use RuntimeException;
-use Swoole\Coroutine\WaitGroup;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
-
-use function Swoole\Coroutine\go;
 
 #[Singleton]
 readonly class StaticPageProcessor
@@ -32,42 +28,24 @@ readonly class StaticPageProcessor
         $filesystem = new Filesystem();
         $filesystem->remove($removableFiles);
 
-        // Render pages using layouts and the metadata..
-        // Wrapped in coroutines because it can generate a lot of IO operations.
-
-        $staticPagesCount = $this->staticPageAggregate->staticPages->count();
-        $waitGroup = new WaitGroup($staticPagesCount);
-
         foreach ($this->staticPageAggregate->staticPages as $staticPage) {
             /**
              * @var false|int $cid
              */
-            $cid = go(function () use ($filesystem, $staticPage, $waitGroup) {
-                $outputDirectory = $staticPage->getOutputDirectory();
-                $outputFilename = $staticPage->getOutputPathname();
+            $outputDirectory = $staticPage->getOutputDirectory();
+            $outputFilename = $staticPage->getOutputPathname();
 
-                $filesystem->mkdir($outputDirectory);
+            $filesystem->mkdir($outputDirectory);
 
-                $fhandle = fopen($outputFilename, 'w');
+            $fhandle = fopen($outputFilename, 'w');
 
-                try {
-                    foreach ($this->staticPageLayoutAggregate->render($staticPage) as $contentChunk) {
-                        fwrite($fhandle, $contentChunk);
-                    }
-                } finally {
-                    $waitGroup->done();
-                    fclose($fhandle);
+            try {
+                foreach ($this->staticPageLayoutAggregate->render($staticPage) as $contentChunk) {
+                    fwrite($fhandle, $contentChunk);
                 }
-            });
-
-            if (!is_int($cid)) {
-                throw new RuntimeException('Unable to start a session write coroutine.');
+            } finally {
+                fclose($fhandle);
             }
-        }
-
-        // Wait 100 miliseconds per page
-        if (!$waitGroup->wait($staticPagesCount * 0.1)) {
-            throw new RuntimeException('Static pages wait group took too long to finish.');
         }
 
         // Generate a sitemap
