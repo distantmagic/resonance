1d482ff5f46fd8647f4cc4f94ebce46e59c6e96d
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2024-01-05 19:00:09 +0100

    feat: watch command

---
+++ src/Command/Watch.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Distantmagic\Resonance\Command;
+
+use Distantmagic\Resonance\Attribute\ConsoleCommand;
+use Distantmagic\Resonance\Command;
+use Distantmagic\Resonance\InotifyIterator;
+use Psr\Log\LoggerInterface;
+use RuntimeException;
+use Swoole\Process;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[ConsoleCommand(
+    name: 'watch',
+    description: 'Watch project files for changes (needs inotify)'
+)]
+final class Watch extends Command
+{
+    private ?Process $process = null;
+
+    public function __construct(
+        private LoggerInterface $logger,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('name', InputArgument::REQUIRED, 'What command do you want to run?');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        if (!extension_loaded('inotify')) {
+            throw new RuntimeException('You need to install "inotify" extension');
+        }
+
+        /**
+         * @var string $childCommandName
+         */
+        $childCommandName = $input->getArgument('name');
+
+        $directories = [
+            DM_APP_ROOT,
+            DM_APP_ROOT.'/../config.ini',
+            DM_PUBLIC_ROOT,
+            DM_RESONANCE_ROOT,
+        ];
+
+        $this->restartChildCommand($childCommandName);
+
+        foreach (new InotifyIterator($directories) as $event) {
+            $this->logger->info(sprintf('watch_file_changed(%s)', $event['name']));
+
+            $this->restartChildCommand($childCommandName);
+        }
+
+        return Command::SUCCESS;
+    }
+
+    private function restartChildCommand(string $childCommandName): void
+    {
+        $this->logger->info(sprintf('watch_run(%s)', $childCommandName));
+
+        /**
+         * @var null|int $pid
+         */
+        $pid = $this->process?->pid;
+
+        if (is_int($pid)) {
+            Process::kill($pid, SIGTERM);
+            $this->process = null;
+        }
+
+        $this->process = new Process(static function (Process $worker) use ($childCommandName) {
+            /**
+             * @psalm-suppress InvalidArgument false positive
+             * @psalm-suppress InvalidCast false positive
+             */
+            $worker->exec(PHP_BINARY, [
+                DM_APP_ROOT.'/../bin/resonance.php',
+                $childCommandName,
+            ]);
+        });
+
+        $this->process->start();
+        $this->process->wait(false);
+    }
+}
---
+++ src/InotifyIterator.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Distantmagic\Resonance;
+
+use Generator;
+use IteratorAggregate;
+use RuntimeException;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * @template-implements IteratorAggregate<array{
+ *   cookie: int,
+ *   mask: int,
+ *   name: string,
+ *   wd: int,
+ * }>
+ */
+readonly class InotifyIterator implements IteratorAggregate
+{
+    /**
+     * @param array<string> $paths
+     */
+    public function __construct(
+        private array $paths,
+        private int $flags = IN_ATTRIB | IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVE,
+    ) {}
+
+    /**
+     * @return Generator<array{
+     *   cookie: int,
+     *   mask: int,
+     *   name: string,
+     *   wd: int,
+     * }>
+     */
+    public function getIterator(): Generator
+    {
+
+        $inotify = inotify_init();
+
+        try {
+            /**
+             * @var array<int> $watched
+             */
+            $watched = [];
+
+            foreach ($this->findPaths() as $path) {
+                $watched[] = inotify_add_watch($inotify, $path, $this->flags);
+            }
+
+            $previous = null;
+            $previousTimeMilis = 0;
+
+            while (true) {
+                $events = inotify_read($inotify);
+
+                if (is_array($events)) {
+                    $currentTimeMilis = $this->getCurrentMiliseconds();
+
+                    /**
+                     * @var array{
+                     *   cookie: int,
+                     *   mask: int,
+                     *   name: string,
+                     *   wd: int,
+                     * } $event
+                     */
+                    foreach ($events as $event) {
+                        $timeDiffMilis = $currentTimeMilis - $previousTimeMilis;
+
+                        if ($previous != $event || $timeDiffMilis > 50) {
+                            yield $event;
+                        }
+
+                        $previous = $event;
+                        $previousTimeMilis = $currentTimeMilis;
+                    }
+                }
+            }
+
+            foreach ($watched as $descriptor) {
+                if (!inotify_rm_watch($inotify, $descriptor)) {
+                    throw new RuntimeException('Unable to remove watched file');
+                }
+            }
+        } finally {
+            fclose($inotify);
+        }
+    }
+
+    /**
+     * @return Generator<string>
+     */
+    private function findDirectories(): Generator
+    {
+        foreach ($this->paths as $path) {
+            if (is_dir($path)) {
+                yield $path;
+            }
+        }
+    }
+
+    /**
+     * @return Generator<string>
+     */
+    private function findPaths(): Generator
+    {
+        $finder = new Finder();
+        $found = $finder
+            ->directories()
+            ->in(iterator_to_array($this->findDirectories()))
+        ;
+
+        foreach ($found as $path) {
+            yield $path->getPathname();
+        }
+
+        foreach ($this->paths as $path) {
+            yield $path;
+        }
+    }
+
+    private function getCurrentMiliseconds(): float
+    {
+        return floor(microtime(true) * 1000);
+    }
+}
