d73680cd5283280b71ddb1c78fd09ab76ff92025
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2024-02-13 06:08:02 +0100

    fix: invalid feature provider

--- src/SingletonProvider/ConfigurationProvider/LlamaCppConfigurationProvider.php
+++ src/SingletonProvider/ConfigurationProvider/LlamaCppConfigurationProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Distantmagic\Resonance\SingletonProvider\ConfigurationProvider;
 
-use Distantmagic\Resonance\Attribute\GrantsFeature;
 use Distantmagic\Resonance\Attribute\Singleton;
 use Distantmagic\Resonance\Constraint;
 use Distantmagic\Resonance\Constraint\EnumConstraint;
@@ -12,7 +11,6 @@ use Distantmagic\Resonance\Constraint\IntegerConstraint;
 use Distantmagic\Resonance\Constraint\NumberConstraint;
 use Distantmagic\Resonance\Constraint\ObjectConstraint;
 use Distantmagic\Resonance\Constraint\StringConstraint;
-use Distantmagic\Resonance\Feature;
 use Distantmagic\Resonance\LlamaCppConfiguration;
 use Distantmagic\Resonance\SingletonProvider\ConfigurationProvider;
 
@@ -25,7 +23,6 @@ use Distantmagic\Resonance\SingletonProvider\ConfigurationProvider;
  *     scheme: non-empty-string,
  * }>
  */
-#[GrantsFeature(Feature::Grpc)]
 #[Singleton(provides: LlamaCppConfiguration::class)]
 final readonly class LlamaCppConfigurationProvider extends ConfigurationProvider
 {
