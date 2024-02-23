f28de193f6379a2be47d95d339efdc88d76acf8f
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2024-02-23 20:57:57 +0100

    feat: formatted query logs

--- src/DatabaseQueryLogger.php
+++ src/DatabaseQueryLogger.php
@@ -4,19 +4,24 @@ declare(strict_types=1);
 
 namespace Distantmagic\Resonance;
 
+use Doctrine\SqlFormatter\SqlFormatter;
 use Psr\Log\LoggerInterface;
 
 readonly class DatabaseQueryLogger
 {
+    private SqlFormatter $sqlFormatter;
+
     public function __construct(
         private DatabaseConnectionPoolConfiguration $databaseConnectionPoolConfiguration,
         private LoggerInterface $logger,
-    ) {}
+    ) {
+        $this->sqlFormatter = new SqlFormatter();
+    }
 
     public function onQueryBeforeExecute(string $sql): void
     {
         if ($this->databaseConnectionPoolConfiguration->logQueries) {
-            $this->logger->debug($sql);
+            $this->logger->debug($this->sqlFormatter->format($sql));
         }
     }
 }
