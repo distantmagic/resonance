5949a70469806e27da4c05e2bdc04c8f2ed586a2
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2024-04-08 20:37:55 +0200

    chore: dialogue controller root node

--- src/DialogueController.php
+++ src/DialogueController.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 namespace Distantmagic\Resonance;
 
-readonly class DialogueController implements DialogueControllerInterface {}
+abstract readonly class DialogueController implements DialogueControllerInterface {}
--- src/DialogueControllerInterface.php
+++ src/DialogueControllerInterface.php
@@ -4,4 +4,7 @@ declare(strict_types=1);
 
 namespace Distantmagic\Resonance;
 
-interface DialogueControllerInterface {}
+interface DialogueControllerInterface
+{
+    public function getRootDialogueNode(): DialogueNodeInterface;
+}
